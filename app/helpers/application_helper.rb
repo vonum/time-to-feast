1dfc1d2df1e5dc676b1d501bbd4fd20088dc67f0
vonum <milan.keca7@gmail.com>
2016-02-16 16:14:19 +0100

    added helper for rendering links to add and remove friends

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+
+	def link_for_user user
+		if current_user.friends_with?(user)
+			button_to 'Remove friend', friendship_path(user), method: :delete
+		else
+			link_to 'Send request', send_request_friendship_path(user)
+		end
+	end
 end
