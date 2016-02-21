34a6d98ffb9c41e1bf5244ac71f544091789d9a0
vonum <milan.keca7@gmail.com>
2016-02-21 20:08:42 +0100

    small changes to date validation

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -14,14 +14,14 @@ class ReservationsController < ApplicationController
 		@reservation = @table.reservations.build(reservation_params)
 		@reservation.user_id = current_user.id
 
-		if check_dates(@reservation.finish, @reservation.start)
+		if check_dates(@reservation.start, @reservation.finish, @reservation.date)
 			if @reservation.save
-				redirect_to root_path
+				redirect_to reservations_users_path
 			else
 				redirect_to action: 'new'
 			end
 		else
-			redirect_to reservations_users_path
+			redirect_to action: 'new'
 		end
 
 	end
@@ -30,7 +30,11 @@ class ReservationsController < ApplicationController
 	def reservation_params
 		params.require(:reservation).permit(:date, :start, :finish)
 	end
-	private def check_dates finish, start
-		return (TimeDifference.between(finish, start).in_minutes > 30 and finish > start)
+	private def check_dates start, finish, date
+		return (TimeDifference.between(finish, start).in_minutes > 30 and finish > start and date > Date.today)
+	end
+	private
+	def table_free table
+
 	end
 end
