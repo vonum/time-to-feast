5e1d3beb923fa19e3e6c1eaea1e0ec2d06214d57
vonum <milan.keca7@gmail.com>
2016-02-22 00:56:05 +0100

    validation from database if table is free at reservations

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -14,7 +14,7 @@ class ReservationsController < ApplicationController
 		@reservation = @table.reservations.build(reservation_params)
 		@reservation.user_id = current_user.id
 
-		if check_dates(@reservation.start, @reservation.finish, @reservation.date)
+		if check_dates(@reservation.start, @reservation.finish, @reservation.date) and table_free(@table, @reservation)
 			if @reservation.save
 				redirect_to reservations_users_path
 			else
@@ -31,10 +31,16 @@ class ReservationsController < ApplicationController
 		params.require(:reservation).permit(:date, :start, :finish)
 	end
 	private def check_dates start, finish, date
-		return (TimeDifference.between(finish, start).in_minutes > 30 and finish > start and date > Date.today)
+		return (TimeDifference.between(finish, start).in_minutes >= 30 and finish > start and date >= Date.today)
 	end
 	private
-	def table_free table
-
+	def table_free table, reservation
+		reservations = Reservation.where(table_id: reservation.table_id, date: reservation.date)
+		reservations.each do |res|
+			if ((res.finish >= reservation.start and res.finish <= reservation.finish) or (res.start >= reservation.start and res.start <= reservation.finish))
+				return false
+			end 
+		end
+		return true
 	end
 end
