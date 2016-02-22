27818eee6ae46a31130c0d889469ec6e9cc0ca0e
vonum <milan.keca7@gmail.com>
2016-02-22 14:15:18 +0100

    added validation for uniqueness for invites, you cant invite same user for same reservation

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,4 +1,5 @@
 class ReservationsController < ApplicationController
+	before_action :authenticate_user!
 	def index
 		@restaurant = Restaurant.find(params[:restaurant_id])
 		@table = Table.find(params[:table_id])
